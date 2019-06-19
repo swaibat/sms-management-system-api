4789d71f3ac00e9207060eb50abcac61c294acef
swaibu <sswaib11@gmail.com>
2019-06-19 16:02:23 +0300

    feature(signup):implement user signup

---
+++ api/data/users.js
@@ -0,0 +1,61 @@
+export const users = [
+    {
+      status: 200,
+      data: {
+        id: '1',
+        email: 'job@gmail.com',
+        firstName: 'job',
+        lastName: 'ogins',
+        password: 'december',
+        address: 'kigali',
+        phoneNumber: "+23436728282992",
+        isAdmin: true,
+      },
+    },
+    {
+      status: 200,
+      data: {
+        id: '2',
+        email: 'andy@gmail.com',
+        firstName: 'andy',
+        lastName: 'mona',
+        password: 'january',
+        address: 'hoima',
+        phoneNumber: "+23436728282992",
+        isAdmin: false,
+      },
+    },
+    {
+      status: 200,
+      data: {
+        id: '3',
+        email: 'bob@gmail.com',
+        firstName: 'bob',
+        lastName: 'ogins',
+        password: 'anderson',
+        address: 'kigali',
+        phoneNumber: "+23436728282992",
+        status: 'un-verified',
+        isAdmin: false,
+      },
+    },
+  
+  ];
+  
+  export const testdata = {
+    email: "and@gmail.com",
+    firstName: "andy",
+    lastName: "mona",
+    password: "january",
+    phoneNumber: "+23436728282992",
+    address: "hoima"
+  }
+  
+  export const testlength = {
+    email: "an@gg.cc",
+    firstName: "an",
+    lastName: "mona",
+    password: "january",
+    phoneNumber: "+23436728282992",
+    address: "hoima"
+  }
